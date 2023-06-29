c14d7ea6a7c0c812167446c6311b3ff51e0d815f
muhammad yulasfi pahrizal <muhammadpahrizal790@gmail.com>
2023-06-29 19:00:36 +0800

    added authentication and resource protection

--- migrations/1688028016616_create-table-authentications.js
+++ migrations/1688028016616_create-table-authentications.js
@@ -1,7 +1,18 @@
 /* eslint-disable camelcase */
 
+/* eslint-disable camelcase */
+
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
